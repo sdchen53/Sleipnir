2d1052ee902bdd83b43962be7a566cfbed041b93
sdchen53 <s_chen53@mail.fhsu.edu>
2026-01-12 22:07:31 +0800

    Update web_v2.py

--- web_v2.py
+++ web_v2.py
@@ -369,8 +369,8 @@ default_character_presets = {
     # "final": "You are a final decision maker, aggregating insights from multiple agents to provide a comprehensive investment strategy."
     }
 leader_preset = "You are a leader decision maker, aggregating insights from multiple agents to provide a comprehensive investment strategy."
-anything_api = "1DV9A3A-SFFM1XR-QF4TYMR-HZ5X8RY"
-entrypoint = "http://10.201.35.124:3001/api/v1/"
+anything_api = "……"
+entrypoint = "……"
 message_preset = "@agent Get {} stock info, from {} to {}, predict the later day's price, and give the buy-in or hold or sell-out decision on {}, with confidence."
 message_outformat = "Current holding is {} shares, max holding is {} shares. Trade limit per operation is{}, expected return percentage is {}. Answer MUST contain Example style: '{}, buy-in, 0.5, hold, 0.1, sell-out, 0.4, hands-in, 200, hands-off, 100'"
 
